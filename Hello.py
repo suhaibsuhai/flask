e67c03e411f6387456980c721cd488816f64147a
suhaibsuhai <152302447+suhaibsuhai@users.noreply.github.com>
2023-12-10 00:19:52 +0000

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
-@app.route("/")#URL leading to method
-def hello(): # Name of the method
- return("Hello World!") #indent this line
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
 if __name__ == "__main__":
- app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
+    cert_path = os.path.join(os.path.dirname(__file__), 'cert.pem')
+    key_path = os.path.join(os.path.dirname(__file__), 'privkey.pem')
+    
+    app.run(host='0.0.0.0', port='8080', ssl_context=(cert_path, key_path))
